ec51239fd9aa94eda9ccea320e578ca21fa52283
shvetsovdm <sorrows@mail.ru>
2017-04-02 17:54:56 +1000

    Add proof of the concept

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "os"
+)
+
+var Tformat = time.Kitchen
+var SecInHour = 3600
+
+func toutput(time time.Time) {
+  fmt.Printf("Your time is: %v\n", time.Format(Tformat))
+}
+
+func doutput(ltime, rtime time.Time) {
+  _, loffset := ltime.Zone()
+  _, roffset := rtime.Zone()
+  if loffset < roffset {
+    fmt.Printf("Difference is: %v hour(s)\n", (loffset - roffset) / SecInHour)
+  } else {
+    fmt.Printf("Difference is: %v hour(s)\n", (roffset - loffset) / SecInHour)
+  }
+}
+
+func main() {
+  utime := time.Now()
+
+  othertimezone, err := time.LoadLocation("Europe/London")
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+
+  toutput(utime)
+  toutput(utime.In(othertimezone))
+  doutput(utime, utime.In(othertimezone))
+}
